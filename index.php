9c2fa846148c91f094a0e0903cd872c30bec17dc
GRANDINMaxime <78095261+GRANDINMaxime@users.noreply.github.com>
2021-10-17 22:54:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $partenaire = new Partner();
 $partenaire->setPartnerName("Maxime");
 
 $casting = new Casting("Casting1");
-$partenaire->setCastingName("Casting1");
+$casting->setCastingName("Casting1");
 
 $partenaire->subscribe($casting);
 
